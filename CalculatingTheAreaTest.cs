f42c1f228502cacc38d9a93c9f195a04d280b80b
NePalDevelop <96785307+NePalDevelop@users.noreply.github.com>
2022-01-27 14:47:10 +0300

    Create CalculatingTheAreaTest.cs
    Tests for СalculatingTheArea

---
+++ CalculatingTheAreaTest.cs
@@ -0,0 +1,90 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace СalculatingTheArea.Tests
+{
+    [TestClass()]
+    public class CalculatingTheAreaTest
+    {
+        [TestMethod()]
+        public void GetAreaCircle_2_return_4PI()
+        {
+            double radius = 2.0;
+            double expected = 4 * Math.PI;
+
+            Circle c = FigureFactory.CreateCircle(radius);
+            double actual = c.GetArea();
+
+            Assert.AreEqual(expected, actual, 0.0001);
+
+        }
+
+        [TestMethod()]
+        public void CreateCircle_3_return_not_null()
+        {
+            double radius = 3.0;
+
+            Assert.IsNotNull(FigureFactory.CreateCircle(radius));
+
+        }
+
+        [TestMethod()]
+        public void CreateCircle_input_negative_Radius_return_null()
+        {
+            double radius = -1.0;
+
+            Assert.IsNull(FigureFactory.CreateCircle(radius));
+
+        }
+
+        [TestMethod()]
+        public void GetAreaTriangle_3and4and5_return_6()
+        {
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 5.0;
+            double expected = 6.0;
+
+            Triangle t = FigureFactory.CreateTriangle(side1, side2, side3);
+            double actual = t.GetArea();
+
+            Assert.AreEqual(expected, actual, 0.0001);
+
+        }
+
+        [TestMethod()]
+        public void CreateTriangle_input_negative_side_return_null()
+        {
+            double side1 = 3.0;
+            double side2 = -4.0;
+            double side3 = 5.0;
+
+            Assert.IsNull(FigureFactory.CreateTriangle(side1, side2, side3));
+
+        }
+
+        [TestMethod()]
+        public void CreateTriangle_input_noncorrect_sides_return_null()
+        {
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 8.0;
+
+            Assert.IsNull(FigureFactory.CreateTriangle(side1, side2, side3));
+
+        }
+
+
+        [TestMethod()]
+        public void CreateTriangle_3and4and5_return_not_null()
+        {
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 5.0;
+
+            Assert.IsNotNull(FigureFactory.CreateTriangle(side1, side2, side3));
+
+        }
+
+    }
+}
